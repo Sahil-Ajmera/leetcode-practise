338c3739a4f372ce96d3b52858f438793f2f721d
Sahil Ajmera <32546795+Sahil-Ajmera@users.noreply.github.com>
2021-11-12 13:24:09 -0800

    Update count_good_nodes_binary_tree.py

--- count_good_nodes_binary_tree.py
+++ count_good_nodes_binary_tree.py
@@ -49,3 +49,28 @@ class Solution:
         result = []
         traverseNodeWithPath(root, [])
         return len(result)
+      
+ 
+
+"""
+Solution storing the max at every node
+
+Time Complexity : O(N)
+Space complexity: O(N) call stack is as high as height of the tree
+"""
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+	
+        def dfs(node, max_so_far):
+            nonlocal num_good_nodes
+            if max_so_far <= node.val:
+                num_good_nodes += 1
+            if node.right:
+                dfs(node.right, max(node.val, max_so_far))
+            if node.left:
+                dfs(node.left, max(node.val, max_so_far))
+        
+        num_good_nodes = 0
+        dfs(root, float("-inf"))
+        return num_good_nodes
